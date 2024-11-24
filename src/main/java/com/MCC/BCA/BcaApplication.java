d4819e3dd5e7b66579866ea17c757cc00d954a92
Ajin Varughese <ajinvarughese91@gmail.com>
2024-11-24 17:43:19 +0530

    update

--- src/main/java/com/MCC/BCA/BcaApplication.java
+++ src/main/java/com/MCC/BCA/BcaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@CrossOrigin("https://mccbca.netlify.app")
+@CrossOrigin(origins = "https://mccbca.netlify.app")
 public class BcaApplication {
 
 	public static void main(String[] args) {
