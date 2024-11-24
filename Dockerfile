b7920fdccd27541896bf6aa231e5fdd5ce9c3f95
Ajin Varughese <ajinvarughese91@gmail.com>
2024-11-24 17:16:30 +0530

    update

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:23
 EXPOSE 8080
-ADD target/mccbca.jar mccbca.jar
+ADD jar/mccbca.jar mccbca.jar
 ENTRYPOINT ["java","-jar","/mccbca.jar"]
--- src/main/java/com/MCC/BCA/configuration/Config.java
+++ src/main/java/com/MCC/BCA/configuration/Config.java
@@ -13,7 +13,7 @@ public class Config {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://mccbca.netlify.app")
                         .allowedMethods("POST", "PATCH", "DELETE", "GET");
             }
         };
